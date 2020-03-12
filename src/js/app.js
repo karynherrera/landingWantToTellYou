28bcdcd7eda350c3df29afcc02bdcabe347400da
Karyn Herrera Castro <kherrera@iconstruye.com>
2020-03-12 13:34:42 -0300

    se añade funcionalidad show more

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     
 })
+/*
+$('.btn-show-more').on("click", function () {
+    console.log($(this).text());
+})
+*/
+$('.btn-show-more').click(function(){
+    $(this).text(function(i,old){
+        return old=='VER MENOS' ?  'VER MÁS' : 'VER MENOS';
+    });
+});
