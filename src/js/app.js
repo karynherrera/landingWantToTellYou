5c77e7b95dacba5361908cf7fe0c033f5cb891d4
Karyn Herrera Castro <kherrera@iconstruye.com>
2020-03-12 12:33:47 -0300

    se añade animacion primer item

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,3 @@
-
-var figure = $(".video").hover( hoverVideo, hideVideo );
-
-function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
-}
-
-function hideVideo(e) {
-    $('video', this).get(0).pause(); 
-}
+$(document).ready(function() {
+    
+})
