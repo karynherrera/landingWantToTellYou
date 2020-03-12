e86ca0a547eb0ce9fe84f9bc49eed067023db00e
Karyn Herrera Castro <kherrera@iconstruye.com>
2020-03-12 15:23:46 -0300

    se añaden el resto de animaciones

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     
 })
-/*
-$('.btn-show-more').on("click", function () {
-    console.log($(this).text());
-})
-*/
+
 $('.btn-show-more').click(function(){
     $(this).text(function(i,old){
         return old=='VER MENOS' ?  'VER MÁS' : 'VER MENOS';
